865fa9deda0dc5d679d87881a8a72e82b65c8109
Kermid <jakub.gibowski312@gmail.com>
2025-04-14 12:05:58 +0200

    first commit

---
+++ calculator/src/Calculator.jsx
@@ -0,0 +1,43 @@
+import React,{useState} from "react"; 
+
+function Calculator()
+{
+    const [input, setInput] = useState("");
+    const [outcome, setOutcome] = useState("");
+    
+    function addChar(number)
+    {
+        setOutcome("");
+        setInput(o => o + number);
+        console.log(outcome);
+    }
+    function Calculate()
+    {
+        setOutcome(eval(input));
+        setInput("");
+    }
+    function Clear()
+    {
+        setOutcome("");
+        setInput("");
+    }
+    return(
+        <>
+        <div className="outcome">
+            <p className="outcome">{outcome}</p>
+            <p className="input">{input}</p>
+        </div>
+        <div className="buttons">
+            {"123+456-789*/0".split("").map((char) => (
+                <button key={char} className=""onClick={() => addChar(char)}>
+                    {char}
+                </button>
+            ))}
+            <button onClick={Calculate}>=</button>
+            <button onClick={Clear}>C</button>
+        </div>
+        
+        </>
+    )
+}
+export default Calculator
